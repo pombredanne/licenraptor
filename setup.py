b0676f7f0e4e74ebde76838e8d5bf713fc787ec0
Hong-She Liang <starofrainnight@gmail.com>
2018-06-30 16:25:14 +0800

    Removed python 3.3 support

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
