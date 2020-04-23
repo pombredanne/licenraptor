fddfdd5a829853ac8c1a0a129bfd627a7f177c76
Hong-She Liang <starofrainnight@gmail.com>
2020-04-23 23:52:07 +0800

    Drop supports of python 3.4

--- setup.py
+++ setup.py
@@ -5,55 +5,54 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst', encoding='utf-8') as readme_file, \
-        open('HISTORY.rst', encoding='utf-8') as history_file:
-    long_description = (readme_file.read() + "\n\n" + history_file.read())
+with open("README.rst", encoding="utf-8") as readme_file, open(
+    "HISTORY.rst", encoding="utf-8"
+) as history_file:
+    long_description = readme_file.read() + "\n\n" + history_file.read()
 
 install_requires = [
-    'click>=6.0',
-    'jinja2',
+    "click>=6.0",
+    "jinja2",
 ]
 
 setup_requires = [
-    'pytest-runner',
+    "pytest-runner",
     # TODO(starofrainnight): put setup requirements (distutils extensions, etc.) here
 ]
 
 tests_requires = [
-    'pytest',
+    "pytest",
     # TODO: put package test requirements here
 ]
 
 setup(
-    name='licenraptor',
-    version='0.2.2',
+    name="licenraptor",
+    version="0.2.2",
     description="Library that encapsulates free software licenses",
     long_description=long_description,
     author="Hong-She Liang",
-    author_email='starofrainnight@gmail.com',
-    url='https://github.com/starofrainnight/licenraptor',
+    author_email="starofrainnight@gmail.com",
+    url="https://github.com/starofrainnight/licenraptor",
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'licenraptor=licenraptor.__main__:main'
-        ]
+        "console_scripts": ["licenraptor=licenraptor.__main__:main"]
     },
     include_package_data=True,
     install_requires=install_requires,
     license="Apache Software License",
     zip_safe=False,
-    keywords='licenraptor,licenraptor',
+    keywords="licenraptor,licenraptor",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    test_suite='tests',
+    test_suite="tests",
     tests_require=tests_requires,
     setup_requires=setup_requires,
 )
